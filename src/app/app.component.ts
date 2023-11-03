ecf5a859a52995c4d9aa5a4e6d4403c7f04ff9d2
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-11-02 18:29:38 -0700

    Added Bootstrap and set prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,21 @@ export class AppComponent {
   result = '';
   searchTerm: string = '';
   searchResult: string = '';
+  bulletPoints: string[] = [];
 
   constructor(private http: HttpClient) {}
+
+  getBasicCareInformation = async () => {
+    const url = `${environment.apiUrl}/api/send-data`
+    this.isLoading = true;
+    const prompt = `Provide the ideal care conditions for a ${this.searchTerm}. Answer directly with only key value pairs. Use US standareds for all units.`
+
+    await this.http.post<MyResponse>(url, { data: prompt })
+      .subscribe((response) => {
+        this.isLoading = false;
+        this.bulletPoints = response.message.content.split(/\n/g);
+      });
+  }
 
   sendDataToBackend = async () => {
     const url = `${environment.apiUrl}/api/send-data`
