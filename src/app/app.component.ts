42dbdb18049e42d05f52d34a4ac265a783e573bd
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-11-04 21:18:02 -0700

    Worked on chat window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,32 +15,56 @@ interface ResponseObject {
 export class AppComponent {
   title = 'PetGPT';
   isLoading = false;
+  messages: any = ["Hello! What type of animal is your pet?"];
   result = '';
+  userInput: string = '';
+  initialMessage: boolean = true;
   searchTerm: string = '';
   searchResult: string = '';
   bulletPoints: string[] = [];
+  showIncorrectInformationButton: boolean = false;
+  showAskQuestionButton: boolean = false;
 
   constructor(private http: HttpClient) {}
+
+  isStringArray(message: any): boolean {
+    return Array.isArray(message);
+  }
 
   getBasicCareInformation = async () => {
     const url = `${environment.apiUrl}/api/send-data`
     this.isLoading = true;
-    const prompt = `Provide the ideal care conditions for a ${this.searchTerm}. Answer directly with only key value pairs. Use US standareds for all units.`
+    this.messages.push("Typing...");
+    const prompt = `Provide the ideal care conditions for a ${this.userInput}. Answer directly with only key value pairs; do not use extra symbols. Use US standards for all units. Include the Latin name among the key value pairs.`
 
     await this.http.post<MyResponse>(url, { data: prompt })
       .subscribe((response) => {
         this.isLoading = false;
-        this.bulletPoints = response.message.content.split(/\n/g);
+        this.bulletPoints = response.message.content.split(/\n/g).filter(x => x.length > 0);
+        console.log(this.bulletPoints);
+        this.messages.pop();
+        this.messages.push(this.bulletPoints)
       });
+  }
+
+  sendMessage = () => {
+    this.messages.push(this.userInput);
+    if (this.initialMessage) {
+      this.getBasicCareInformation();
+      this.initialMessage = false;
+    } else {
+      this.sendDataToBackend();
+    }
+    this.userInput = '';
   }
 
   sendDataToBackend = async () => {
     const url = `${environment.apiUrl}/api/send-data`
     this.isLoading = true;
-    await this.http.post<MyResponse>(url, { data: this.searchTerm })
+    await this.http.post<MyResponse>(url, { data: this.userInput })
       .subscribe((response) => {
         this.isLoading = false;
-        this.searchResult = response.message.content;
+        this.messages.push(response.message.content);
       });
   }
 
