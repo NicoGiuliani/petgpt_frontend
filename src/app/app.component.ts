dd1036f1b789f1eb17030f074b347af893e241c3
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-11-04 23:51:23 -0700

    Worked on chat window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment'
 import { MyResponse } from './interfaces';
@@ -12,7 +12,10 @@ interface ResponseObject {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('chatMessages') chatMessages: ElementRef;
+
+
   title = 'PetGPT';
   isLoading = false;
   messages: any = ["Hello! What type of animal is your pet?"];
@@ -25,7 +28,23 @@ export class AppComponent {
   showIncorrectInformationButton: boolean = false;
   showAskQuestionButton: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.chatMessages = new ElementRef(null);
+  }
+
+  ngOnInit() { 
+        this.scrollToBottom();
+    }
+
+    ngAfterViewChecked() {        
+        this.scrollToBottom();        
+    } 
+
+    scrollToBottom(): void {
+        try {
+            this.chatMessages.nativeElement.scrollTop = this.chatMessages.nativeElement.scrollHeight;
+        } catch(err) { }                 
+    }
 
   isStringArray(message: any): boolean {
     return Array.isArray(message);
@@ -35,20 +54,22 @@ export class AppComponent {
     const url = `${environment.apiUrl}/api/send-data`
     this.isLoading = true;
     this.messages.push("Typing...");
-    const prompt = `Provide the ideal care conditions for a ${this.userInput}. Answer directly with only key value pairs; do not use extra symbols. Use US standards for all units. Include the Latin name among the key value pairs.`
+    const prompt = `Provide the ideal care conditions for a ${this.userInput}. Answer directly with only key value pairs for this message; do not use extra symbols. Use US standards for all units. Include the "Latin name" among the key value pairs.`
 
-    await this.http.post<MyResponse>(url, { data: prompt })
+    await this.http.post<MyResponse>(url, { data: prompt, userMessage: this.userInput })
       .subscribe((response) => {
         this.isLoading = false;
         this.bulletPoints = response.message.content.split(/\n/g).filter(x => x.length > 0);
-        console.log(this.bulletPoints);
+        // console.log(this.bulletPoints);
         this.messages.pop();
         this.messages.push(this.bulletPoints)
+        this.scrollToBottom();
       });
   }
 
   sendMessage = () => {
     this.messages.push(this.userInput);
+    this.scrollToBottom();
     if (this.initialMessage) {
       this.getBasicCareInformation();
       this.initialMessage = false;
@@ -59,12 +80,16 @@ export class AppComponent {
   }
 
   sendDataToBackend = async () => {
+    console.log("sending data to backend...");
     const url = `${environment.apiUrl}/api/send-data`
     this.isLoading = true;
+    this.messages.push("Typing...");
     await this.http.post<MyResponse>(url, { data: this.userInput })
       .subscribe((response) => {
         this.isLoading = false;
+        this.messages.pop();
         this.messages.push(response.message.content);
+        this.scrollToBottom();
       });
   }
 
